26ebcd5429b282c0db18e25fb171f8114b40e560
TAEWOOKK <kto5294@gmail.com>
2024-11-30 15:23:41 +0900

    채팅 리스트 로직 제작중

--- websocket/src/main/java/com/back/websocket/chat/service/ChatService.java
+++ websocket/src/main/java/com/back/websocket/chat/service/ChatService.java
@@ -1,16 +1,19 @@
 package com.back.websocket.chat.service;
 
-import com.back.websocket.chat.dto.ChatSaveDTO;
+import com.back.websocket.chat.dto.ChatListDTO;
 import com.back.websocket.chat.entity.ChatEntity;
 import com.back.websocket.chat.repository.ChatRepository;
-import com.back.websocket.config.dto.StateRes;
+import com.back.websocket.room.entity.RoomEntity;
 import com.back.websocket.room.repository.RoomRepository;
-import com.back.websocket.user.dto.CustomUserDetails;
 import com.back.websocket.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
@@ -19,13 +22,33 @@ public class ChatService {
     private final ChatRepository chatRepository;
     private final RoomRepository roomRepository;
     private final UserRepository userRepository;
+    private final MongoTemplate mongoTemplate;
     
     public void chat_save(String room_id, String email ,String content){
-        
+
+        System.out.println(email);
+
         chatRepository.save(ChatEntity.builder()
                 .content(content)
                 .user(userRepository.findByEmail(email))
                 .room(roomRepository.findById(room_id).orElse(null)).build());
 
+    }
+
+    public List<ChatListDTO> chat_List(String room_id){
+
+        RoomEntity roomEntity = roomRepository.findById(room_id).orElse(null);
+
+        Query query = new Query();
+
+        query.addCriteria(Criteria.where("room.id").is(room_id));
+
+        query.fields().include("content").include("createdAt");
+
+        List<ChatEntity> chats = mongoTemplate.find(query, ChatEntity.class);
+
+        return chats.stream()
+                .map(chat -> new ChatListDTO(chat.getContent(),chat.getCreatedAt()))
+                .toList();
     }
 }
