e74a3e762eaf15a4292e6ff206e3ce2267daf0e4
TAEWOOKK <kto5294@gmail.com>
2024-12-04 17:45:24 +0900

    채팅방 리스트 스타일 수정 및 return 타입 변경

--- websocket/src/main/java/com/back/websocket/chat/controller/ChatController.java
+++ websocket/src/main/java/com/back/websocket/chat/controller/ChatController.java
@@ -5,13 +5,13 @@ import com.back.websocket.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
-@Controller
+import java.util.List;
+
 @Slf4j
+@Controller
 @RequiredArgsConstructor
 public class ChatController {
 
@@ -34,11 +34,10 @@ public class ChatController {
 
     // 사용자에게 개인 메시지 보내기
     @MessageMapping("/chat.addUser")
-    @SendToUser("/queue/reply") // 사용자에게 /user/queue/reply로 메시지를 보내기
-    public ChatMessageDto addUser(ChatMessageDto chatMessageDto) {
-        // 사용자가 채팅방에 들어갔을 때 처리
-        log.info("채팅 유저 추가");
-        chatMessageDto.setMessage(chatService.chat_List(chatMessageDto.getChatRoomId()).toString());
-        return chatMessageDto;
+    public void addUser(ChatMessageDto chatMessageDto) {
+
+        // 클라이언트에게 메시지 전송
+        messagingTemplate.convertAndSendToUser(chatMessageDto.getEmail(),
+                "/queue/reply", chatService.chat_List(chatMessageDto.getChatRoomId()));
     }
 }
