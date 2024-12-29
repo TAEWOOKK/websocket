758f2a3003c0471075701d1847ca73e97710f408
TAEWOOKK <kto5294@gmail.com>
2024-12-29 15:41:24 +0900

    웹소켓 끊김 처리 진짜 됨

--- websocket/src/main/java/com/back/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/back/websocket/config/WebSocketConfig.java
@@ -1,19 +1,22 @@
 package com.back.websocket.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.handler.WebSocketHandlerDecorator;
+import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
+import org.springframework.web.socket.messaging.SubProtocolWebSocketHandler;
+
+import java.io.IOException;
 
 @Configuration
+@Slf4j
 @EnableWebSocketMessageBroker // STOMP를 사용하기 위해 필요한 어노테이션
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    private final StompLoggingInterceptor stompLoggingInterceptor;
-
-    public WebSocketConfig(StompLoggingInterceptor stompLoggingInterceptor) {
-        this.stompLoggingInterceptor = stompLoggingInterceptor;
-    }
-
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -36,8 +39,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void configureClientInboundChannel(org.springframework.messaging.simp.config.ChannelRegistration registration) {
-        // ChannelInterceptor 등록
-        registration.interceptors(stompLoggingInterceptor);
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        // Custom WebSocketHandlerDecorator를 설정하여 연결 종료 시 로그 찍기
+        registration.addDecoratorFactory(new WebSocketHandlerDecoratorFactory() {
+            @Override
+            public WebSocketHandler decorate(WebSocketHandler handler) {
+                return new WebSocketHandlerDecorator(handler) {
+                    @Override
+                    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+                        super.afterConnectionClosed(session, closeStatus);
+                        // 연결 종료 후 로그 출력
+                        log.info("WebSocket 연결 종료: " + session.getId() + " 상태: " + closeStatus);
+                    }
+                };
+            }
+        });
     }
 }
